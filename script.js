e42fe3efc1aa69306ddff306b22de13fce3ac99f
Endi <endre1988@freemail.hu>
2019-09-19 11:23:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,302 +1,311 @@
- function soldunitsrevenue2(){
-	const dataset=[
-  {
-    "Month": "2013_09",
-    "Count": 1
-  },
-  {
-    "Month": "2013_10",
-    "Count": 291
-  },
-  {
-    "Month": "2013_11",
-    "Count": 763
-  },
-  {
-    "Month": "2013_12",
-    "Count": 853
-  },
-  {
-    "Month": "2014_01",
-    "Count": 590
-  },
-  {
-    "Month": "2014_02",
-    "Count": 810
-  },
-  {
-    "Month": "2014_03",
-    "Count": 1159
-  },
-  {
-    "Month": "2014_04",
-    "Count": 1158
-  },
-  {
-    "Month": "2014_05",
-    "Count": 801
-  },
-  {
-    "Month": "2014_06",
-    "Count": 915
-  },
-  {
-    "Month": "2014_07",
-    "Count": 1474
-  },
-  {
-    "Month": "2014_08",
-    "Count": 1153
-  },
-  {
-    "Month": "2014_09",
-    "Count": 1177
-  },
-  {
-    "Month": "2014_10",
-    "Count": 1079
-  },
-  {
-    "Month": "2014_11",
-    "Count": 912
-  },
-  {
-    "Month": "2014_12",
-    "Count": 1123
-  },
-  {
-    "Month": "2015_01",
-    "Count": 554
-  },
-  {
-    "Month": "2015_02",
-    "Count": 965
-  },
-  {
-    "Month": "2015_03",
-    "Count": 995
-  },
-  {
-    "Month": "2015_04",
-    "Count": 1263
-  },
-  {
-    "Month": "2015_05",
-    "Count": 965
-  },
-  {
-    "Month": "2015_06",
-    "Count": 1167
-  },
-  {
-    "Month": "2015_07",
-    "Count": 945
-  },
-  {
-    "Month": "2015_08",
-    "Count": 800
-  },
-  {
-    "Month": "2015_09",
-    "Count": 800
-  },
-  {
-    "Month": "2015_10",
-    "Count": 681
-  },
-  {
-    "Month": "2015_11",
-    "Count": 562
-  },
-  {
-    "Month": "2015_12",
-    "Count": 485
-  },
-  {
-    "Month": "2016_01",
-    "Count": 204
-  },
-  {
-    "Month": "2016_02",
-    "Count": 382
-  },
-  {
-    "Month": "2016_03",
-    "Count": 406
-  },
-  {
-    "Month": "2016_04",
-    "Count": 473
-  },
-  {
-    "Month": "2016_05",
-    "Count": 556
-  },
-  {
-    "Month": "2016_06",
-    "Count": 576
-  },
-  {
-    "Month": "2016_07",
-    "Count": 606
-  },
-  {
-    "Month": "2016_08",
-    "Count": 904
-  },
-  {
-    "Month": "2016_09",
-    "Count": 912
-  },
-  {
-    "Month": "2016_10",
-    "Count": 825
-  },
-  {
-    "Month": "2016_11",
-    "Count": 759
-  },
-  {
-    "Month": "2016_12",
-    "Count": 663
-  },
-  {
-    "Month": "2017_01",
-    "Count": 402
-  },
-  {
-    "Month": "2017_02",
-    "Count": 636
-  },
-  {
-    "Month": "2017_03",
-    "Count": 793
-  },
-  {
-    "Month": "2017_04",
-    "Count": 865
-  },
-  {
-    "Month": "2017_05",
-    "Count": 930
-  },
-  {
-    "Month": "2017_06",
-    "Count": 847
-  },
-  {
-    "Month": "2017_07",
-    "Count": 812
-  },
-  {
-    "Month": "2017_08",
-    "Count": 791
-  },
-  {
-    "Month": "2017_09",
-    "Count": 749
-  },
-  {
-    "Month": "2017_10",
-    "Count": 770
-  },
-  {
-    "Month": "2017_11",
-    "Count": 683
-  },
-  {
-    "Month": "2017_12",
-    "Count": 710
-  },
-  {
-    "Month": "2018_01",
-    "Count": 511
-  },
-  {
-    "Month": "2018_02",
-    "Count": 736
-  },
-  {
-    "Month": "2018_03",
-    "Count": 695
-  },
-  {
-    "Month": "2018_04",
-    "Count": 936
-  },
-  {
-    "Month": "2018_05",
-    "Count": 1013
-  },
-  {
-    "Month": "2018_06",
-    "Count": 946
-  },
-  {
-    "Month": "2018_07",
-    "Count": 994
-  },
-  {
-    "Month": "2018_08",
-    "Count": 964
-  },
-  {
-    "Month": "2018_09",
-    "Count": 872
-  },
-  {
-    "Month": "2018_10",
-    "Count": 945
-  },
-  {
-    "Month": "2018_11",
-    "Count": 754
-  },
-  {
-    "Month": "2018_12",
-    "Count": 895
-  },
-  {
-    "Month": "2019_01",
-    "Count": 600
-  },
-  {
-    "Month": "2019_02",
-    "Count": 913
-  },
-  {
-    "Month": "2019_03",
-    "Count": 1128
-  },
-  {
-    "Month": "2019_04",
-    "Count": 1397
-  },
-  {
-    "Month": "2019_05",
-    "Count": 1304
-  },
-  {
-    "Month": "2019_06",
-    "Count": 1335
-  },
-  {
-    "Month": "2019_07",
-    "Count": 1354
-  },
-  {
-    "Month": "2019_08",
-    "Count": 1307
-  }
-];
+
+
+document.getElementById("refresh").addEventListener("click", soldunitsrevenue2)
+	
+function soldunitsrevenue2(){
+	d3.select("#SVG").remove();
+	var v=document.getElementById("data").value;
+	var obj = JSON.parse(v); //text-ből objektummá
+	/*let dataset=[
+ {
+   "Month": "2013_9",
+   "Count": 20
+ },
+ {
+   "Month": "2013_10",
+   "Count": 574
+ },
+ {
+   "Month": "2013_11",
+   "Count": 822
+ },
+ {
+   "Month": "2013_12",
+   "Count": 515
+ },
+ {
+   "Month": "2014_1",
+   "Count": 809
+ },
+ {
+   "Month": "2014_2",
+   "Count": 944
+ },
+ {
+   "Month": "2014_3",
+   "Count": 1010
+ },
+ {
+   "Month": "2014_4",
+   "Count": 921
+ },
+ {
+   "Month": "2014_5",
+   "Count": 945
+ },
+ {
+   "Month": "2014_6",
+   "Count": 848
+ },
+ {
+   "Month": "2014_7",
+   "Count": 1558
+ },
+ {
+   "Month": "2014_8",
+   "Count": 992
+ },
+ {
+   "Month": "2014_9",
+   "Count": 1112
+ },
+ {
+   "Month": "2014_10",
+   "Count": 1012
+ },
+ {
+   "Month": "2014_11",
+   "Count": 943
+ },
+ {
+   "Month": "2014_12",
+   "Count": 785
+ },
+ {
+   "Month": "2015_1",
+   "Count": 751
+ },
+ {
+   "Month": "2015_2",
+   "Count": 906
+ },
+ {
+   "Month": "2015_3",
+   "Count": 1167
+ },
+ {
+   "Month": "2015_4",
+   "Count": 1066
+ },
+ {
+   "Month": "2015_5",
+   "Count": 974
+ },
+ {
+   "Month": "2015_6",
+   "Count": 1079
+ },
+ {
+   "Month": "2015_7",
+   "Count": 902
+ },
+ {
+   "Month": "2015_8",
+   "Count": 673
+ },
+ {
+   "Month": "2015_9",
+   "Count": 819
+ },
+ {
+   "Month": "2015_10",
+   "Count": 590
+ },
+ {
+   "Month": "2015_11",
+   "Count": 497
+ },
+ {
+   "Month": "2015_12",
+   "Count": 337
+ },
+ {
+   "Month": "2016_1",
+   "Count": 267
+ },
+ {
+   "Month": "2016_2",
+   "Count": 367
+ },
+ {
+   "Month": "2016_3",
+   "Count": 433
+ },
+ {
+   "Month": "2016_4",
+   "Count": 531
+ },
+ {
+   "Month": "2016_5",
+   "Count": 499
+ },
+ {
+   "Month": "2016_6",
+   "Count": 531
+ },
+ {
+   "Month": "2016_7",
+   "Count": 745
+ },
+ {
+   "Month": "2016_8",
+   "Count": 955
+ },
+ {
+   "Month": "2016_9",
+   "Count": 835
+ },
+ {
+   "Month": "2016_10",
+   "Count": 718
+ },
+ {
+   "Month": "2016_11",
+   "Count": 742
+ },
+ {
+   "Month": "2016_12",
+   "Count": 456
+ },
+ {
+   "Month": "2017_1",
+   "Count": 512
+ },
+ {
+   "Month": "2017_2",
+   "Count": 665
+ },
+ {
+   "Month": "2017_3",
+   "Count": 927
+ },
+ {
+   "Month": "2017_4",
+   "Count": 792
+ },
+ {
+   "Month": "2017_5",
+   "Count": 914
+ },
+ {
+   "Month": "2017_6",
+   "Count": 822
+ },
+ {
+   "Month": "2017_7",
+   "Count": 719
+ },
+ {
+   "Month": "2017_8",
+   "Count": 785
+ },
+ {
+   "Month": "2017_9",
+   "Count": 710
+ },
+ {
+   "Month": "2017_10",
+   "Count": 707
+ },
+ {
+   "Month": "2017_11",
+   "Count": 655
+ },
+ {
+   "Month": "2017_12",
+   "Count": 517
+ },
+ {
+   "Month": "2018_1",
+   "Count": 657
+ },
+ {
+   "Month": "2018_2",
+   "Count": 723
+ },
+ {
+   "Month": "2018_3",
+   "Count": 766
+ },
+ {
+   "Month": "2018_4",
+   "Count": 948
+ },
+ {
+   "Month": "2018_5",
+   "Count": 920
+ },
+ {
+   "Month": "2018_6",
+   "Count": 907
+ },
+ {
+   "Month": "2018_7",
+   "Count": 941
+ },
+ {
+   "Month": "2018_8",
+   "Count": 902
+ },
+ {
+   "Month": "2018_9",
+   "Count": 827
+ },
+ {
+   "Month": "2018_10",
+   "Count": 895
+ },
+ {
+   "Month": "2018_11",
+   "Count": 833
+ },
+ {
+   "Month": "2018_12",
+   "Count": 589
+ },
+ {
+   "Month": "2019_1",
+   "Count": 791
+ },
+ {
+   "Month": "2019_2",
+   "Count": 934
+ },
+ {
+   "Month": "2019_3",
+   "Count": 1277
+ },
+ {
+   "Month": "2019_4",
+   "Count": 1108
+ },
+ {
+   "Month": "2019_5",
+   "Count": 1591
+ },
+ {
+   "Month": "2019_6",
+   "Count": 1177
+ },
+ {
+   "Month": "2019_7",
+   "Count": 1415
+ },
+ {
+   "Month": "2019_8",
+   "Count": 1071
+ },
+];*/
+	let dataset=obj;
 	dataset.forEach(d=>d.Count=+d.Count);
-	console.log(dataset[0]);
 	let margin={top:50,right:30,bottom:80,left:60}; 
 	let width= 1200-margin.left-margin.right;
 	let height= 620-margin.top-margin.bottom;
 	
-	const svg = d3.select(".div1")      //svg konstans definiálása            
-	.append("svg")  //svg hozzáadása a div1-hez
+	let svg = d3.select(".div1")      //svg konstans definiálása            
+	.append("svg") 
+	.attr("id","SVG")
+//svg hozzáadása a div1-hez
 	.attr("width", width+margin.left+margin.right)    // az svg szélessége px-ben   
 	.attr("height", height+margin.top+margin.bottom)  // az svg magassága px-ben 
 	.attr("style", "outline: thin solid red;") //külső border hozzáadása
@@ -373,5 +382,5 @@
 	  		.attr("transform","rotate(-40)");
 
   };
+	soldunitsrevenue2();
 
-soldunitsrevenue2();
